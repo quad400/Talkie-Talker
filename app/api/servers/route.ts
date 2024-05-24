ad212c3798a694174db62df9a808f01a44498104
Abdulquadri Adediji <adedijiabdulquadri@gmail.com>
2024-05-24 19:21:39 +0100

    updates

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 
 import { db } from "@/lib/db";
-import currentProfile from "@/lib/current-profile";
+import {currentProfile} from "@/lib/current-profile";
 import { NextResponse } from "next/server";
 import { MemberRole } from "@prisma/client";
 
