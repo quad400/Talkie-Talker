1cb2c898abaf54bd6793231b77b7c8b732706efc
Abdulquadri Adediji <adedijiabdulquadri@gmail.com>
2024-05-24 19:18:12 +0100

    update

--- app/api/servers/[serverId]/invite-code/route.ts
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -2,7 +2,7 @@ import { v4 as uuidV4 } from "uuid";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
-import currentProfile from "@/lib/current-profile";
+import {currentProfile} from "@/lib/current-profile";
 
 export async function PATCH(
   req: Request,
